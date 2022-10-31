78be497352f1bed394b53b0c31558e0227d24483
Spiridon Fotiadis <s.fot@live.com>
2022-10-31 16:11:16 +0100

    checks feeding time with rtc

--- zweites_jahr/LF07/Arduino/auto_feeder/auto_feeder.ino
+++ zweites_jahr/LF07/Arduino/auto_feeder/auto_feeder.ino
@@ -1,14 +1,22 @@
 /*
   Automatic Food Dispenser (WIP)
 
+  Components:
+    
+    + RTC DS3231
+    + cereal dispenser
+    + SG90 servo
+
   created 25 Okt 2022
 
   by Spiridon Fotiadis
 */
 
 #include <Servo.h>
+#include <RTClib.h>
 
 Servo shutter_servo;  // servo object to control the shutter
+RTC_DS3231 rtc;       // real time clock
 
 // servo configuration
 int servo_pin = 9;
@@ -16,8 +24,10 @@ int pos = 0;
 int end_pos = 180;
 int steps = 10;
 
-int day = 1000*60*60*24
+// feeding configuration
+int feedingTime = 19;
 
+// opens shutter for specified amount of miliseconds
 void serve(int miliseconds) {
   for (pos = 0; pos <= end_pos; pos += steps) {  
     shutter_servo.write(pos);             
@@ -32,9 +42,23 @@ void serve(int miliseconds) {
 
 void setup() {
   shutter_servo.attach(servo_pin); 
+
+  while(!Serial);
+
+  Serial.begin(9600);
+  
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  
+  Serial.println("Feeding time set to 19:00");
 }
 
 void loop() {
-  serve(2000);
-  
+  DateTime now = rtc.now();
+
+  if(now.hour() == feedingTime){
+    serve(1000);
+  }
 }
