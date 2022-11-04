0707a0d772e7399898b4f716879275a13fda341e
Spiridon Fotiadis <s.fot@live.com>
2022-11-04 15:02:32 +0100

    adds tests

---
+++ zweites_jahr/LF08/blockworld/blockworld_test.py
@@ -0,0 +1,14 @@
+from pyblockworld import World
+from blockworld import House
+import unittest
+
+class HouseTest(unittest.TestCase):
+    def setUp(self):
+        self.house = House((1,2,3), World())
+        self.house.change_wall_material("default:sand")
+    
+    def test_change_wall_material(self):
+        self.assertEqual(self.house.wallFront.material_id, "default:sand")
+        self.assertEqual(self.house.wallBack.material_id, "default:sand")
+        self.assertEqual(self.house.wallLeft.material_id, "default:sand")
+        self.assertEqual(self.house.wallRight.material_id, "default:sand")
